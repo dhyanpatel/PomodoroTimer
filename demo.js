6c2cf8af89f4749f15ea9bc3ea322a4574c81833
AethenVG <dhyanpp@gmail.com>
2018-04-05 12:19:52 -0500

    removed unecessary comments

--- demo.js
+++ demo.js
@@ -1,11 +1,6 @@
 function startClock(){
 	var myVar = setInterval(timer, 1000)
 }
-//var myVar = setInterval(Timer, 1000);
-//var work = 1200 //seconds for 20 minutes
-//const work = 1200 //seconds for 20 mins
-//const rest = 300 //seconds for 5 mins
-//const longrest = 900 //seconds for 15 mins
 
 const times = [1200, 300, 1200, 300, 1200, 900]
 
